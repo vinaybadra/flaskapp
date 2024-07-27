e72aa191958d7525d034671ecc5e7029ddc6ff21
Vinay Badrasetty <vbadrasetty@zscaler.com>
2024-07-27 19:31:40 +0530

    Added my project

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Bengaluru, India',
+        'salary': 'Rs 10,00,000'
+    },
+    {
+        'id':2,
+        'title':'Data Scientist',
+        'location':'Delhi, India',
+        'salary': 'Rs 15,00,000'
+    },
+    {
+        'id':3,
+        'title':'Frond End Engineer',
+        'location':'Remote',
+        'salary': 'Rs 12,00,000'
+    },
+        {
+        'id':4,
+        'title':'BackEnd Engineer',
+        'location':'San Jose, USA',
+        'salary': '$ 130,000'
+    },
+
+]
+
+@app.route("/")
+def helloworld():
+    return render_template("home.html",jobs = JOBS,company_name = 'VIYA')
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
